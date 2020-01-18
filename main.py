94c7026fdb58637bea99ebe6c8a8ff7531454b94
Павел <pavel-clever@mail.ru>
2020-01-18 13:32:58 +0200

    so we start

---
+++ main.py
@@ -0,0 +1,54 @@
+import re
+import requests
+from telebot import TeleBot, types
+bot = TeleBot('1002398401:AAGBu-ZrLis1wjTKG6RMfoEMqcpAI53cvhI')
+
+
+
+markup = types.ReplyKeyboardMarkup(row_width=2)
+
+@bot.message_handler(commands=['all'])
+def get_definition(message):
+    req = requests.get(
+        f'http://api.urbandictionary.com/v0/define?term={message.text}")')
+
+    text = ""
+
+    for _ in req.json().get('list'):
+        text += _.get('definition')
+
+    result = re.finditer(r'\[\w+\]', text)
+
+    bot.send_message(message.chat.id, text)
+
+    definitions = []
+
+    for _ in result:
+        definitions.append(_.group()[1:-1])
+
+    for word in definitions[:10]:
+        word = types.KeyboardButton(word)
+        markup.add(word)
+
+    bot.send_message(message.chat.id, "Choose a word:",
+                     reply_markup=markup)
+
+    req = requests.get(
+        f'http://api.urbandictionary.com/v0/define?term={message.text}")')
+
+    text = ""
+
+    for _ in req.json().get('list'):
+        text += _.get('definition')
+
+    bot.send_message(message.chat.id, text)
+
+
+if __name__ == '__main__':
+    bot.polling()
+
+
+
+
+
+
